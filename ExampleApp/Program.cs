506448decff4095b83d830924c6efcc08e21560a
Jon Hawkins <jonathan1@houseofhawkins.com>
2012-03-05 20:32:13 +0000

    added solution and example console app showing how the application can be used. Added nunit dependency folder.

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleLoadingOfConfiguration slc = new SimpleLoadingOfConfiguration();
+
+
+            Console.WriteLine("all examples run, press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExampleApp/SimpleLoadingOfConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CustomConfigurations;
+
+namespace ExampleApp
+{
+    public class SimpleLoadingOfConfiguration
+    {
+        public SimpleLoadingOfConfiguration()
+        {
+            Config configLoader = new Config("client2");
+            string myVal = configLoader["keya"];
+            bool result;
+            int myInt = configLoader.TryParse<int>("key2", out result);
+
+            Console.WriteLine("configloader loaded client section called 'client2'");
+            Console.WriteLine("value found for 'key' is: " + myVal);
+            Console.WriteLine(string.Format("attempted try parse for 'key2' to convert to int result '{0}', value {1}", result, myInt));
+
+            Console.WriteLine("");
+        }
+    }
+}
